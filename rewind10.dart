bdd1d6b0ebc8de02eb8ce9b682fcc54576dee47f
cliffordgail01 <davisbeacon23@gmail.com>
2023-06-06 07:43:18 +0300

    Let's GTS

---
+++ rewind10.dart
@@ -0,0 +1,28 @@
+
+void main () {
+
+  final List<Person> persons = List.unmodifiable([
+    Person("cliff", "g"),
+    Person("odhiambo", 'nyanga')
+  ]);
+
+  print(persons);
+  persons[0] = Person("","");
+
+  persons[0].lname = "Gail";
+
+  print(persons);
+}
+
+class Person {
+  String fname;
+  String lname;
+
+  Person(this.fname, this.lname);
+
+  @override
+  String toString() {
+    // TODO: implement toString
+    return "$fname $lname";
+  }
+}
